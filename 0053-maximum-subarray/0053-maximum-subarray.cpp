9956bc81cacf397960a26893e23e8a22b94283eb
Kunal <kunalyadav49101@gmail.com>
2026-08-11 22:09:04 +0530

    Add logic for maximum subarray calculation
    Implement logic to calculate maximum subarray sum.

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,6 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int maxSum =INT_MIN;
         int sum = 0;
+        
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
             maxSum = max(maxSum, sum);
