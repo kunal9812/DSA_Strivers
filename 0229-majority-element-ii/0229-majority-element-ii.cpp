8f3d0e2170b0540992f7b957e3f7fac595407d23
Kunal <kunalyadav49101@gmail.com>
2026-08-24 22:31:32 +0530

    Time: 0 ms (100%), Space: 23.9 MB (47.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,16 +2,33 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
-        map<int,int>freq;
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
-            freq[nums[i]]++;
-        }
-
-        for(auto it: freq){
-            if(it.second > n/3){
-                ans.emplace_back(it.first);
+        int el1,el2,count1 = 0, count2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(count1 == 0 && el2 != nums[i]){
+                count1 = 1;
+                el1 = nums[i];
             }
+            else if(count2 == 0 && el1 != nums[i]){
+                count2 = 1;
+                el2 = nums[i];
+            }
+            else if(el1 == nums[i]) count1++;
+            else if(el2 == nums[i])count2++;
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        count1 = 0,count2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(el1 == nums[i])count1++;
+            else if(el2 == nums[i])count2++;
+        }
+        if(count1>(nums.size())/3){
+            ans.emplace_back(el1);
+        }
+        if(count2>(nums.size())/3){
+            ans.emplace_back(el2);
         }
         return ans;
     }
